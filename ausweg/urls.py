4bdad20cccf1623300185119aa86ac9b133be15e
rakeshroshanmn <rakeshroshan4411@gmail.com>
2021-04-16 08:07:38 +0530

    Small changes

--- ausweg/urls.py
+++ ausweg/urls.py
@@ -22,7 +22,6 @@ from userAus import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='UsersAus/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='UsersAus/logout.html'), name='logout'),
