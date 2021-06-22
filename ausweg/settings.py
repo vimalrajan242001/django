fa1aedc0f28ff7900b2b01883a85b0b102d0ebd0
rakeshroshanmn <rakeshroshan4411@gmail.com>
2021-06-22 09:23:38 +0530

    debug

--- ausweg/settings.py
+++ ausweg/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'web-Home'
