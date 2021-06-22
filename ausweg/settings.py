701c7f585c89c4d63777ed560aa08542033e78f5
rakeshroshanmn <rakeshroshan4411@gmail.com>
2021-06-22 09:28:02 +0530

    Debug

--- ausweg/settings.py
+++ ausweg/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
